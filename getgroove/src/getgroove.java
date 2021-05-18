a8c23c3bbca03cd1798cac6f1d42e4dbf81d3ccd
km4891 <kmorarity@gmail.com>
2021-05-17 20:47:35 -0700

    working on user input

--- getgroove/src/getgroove.java
+++ getgroove/src/getgroove.java
@@ -1,5 +1,96 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.*;
+
+public class getgroove {
+
+    public static class Getgroove {
+
+        
+        Scanner s = new Scanner(System.in);
+        int mood;
+            String[] cryingsolutions = {"Try feeding it", "Change the diaper", "Pick up and walk around", "Take temperature"};
+            String[] fussysolutions = {"Give baby your attention", "Try a nap", "Move or change locations", "Go for a drive", };
+            String[] happysolutions = {"Keep doing what you are doing until your baby's mood changes"};
+            int cryingindex = 0;
+            int fussyindex = 0;
+            int happyindex = 0;
+            String name;
+        
+        Getgroove() {
+            name = s.next();
+        }
+        
+        Getgroove(String name) {
+            this.name = name;
+        }
+        
+        void menu() {
+            System.out.println("What's wrong with " + name + "?");
+            System.out.println("   1) My baby is crying");
+            System.out.println("   2) My baby is fussy");
+            System.out.println("   3) My baby is happpy");
+       
+            mood = s.nextInt();
+            
+            if (mood == 1) {               
+                int sz = cryingsolutions.length;
+                if (cryingindex >= sz) cryingindex = 0;
+                System.out.println("I suggest you " + cryingsolutions[cryingindex]);
+                cryingindex++;
+                
+                System.out.println("Did that solve the issue? (y/n)");
+                char c = s.next().charAt(0);
+                if (c == 'y') mood = 3;
+                
+            } else if (mood == 2) {
+                
+                int sz = fussysolutions.length;
+                if (fussyindex >= sz) fussyindex = 0;
+                System.out.println("I suggest you " + fussysolutions[fussyindex]);
+                fussyindex++;
+                
+                System.out.println("Did that solve the issue? (y/n)");
+                char c = s.next().charAt(0);
+                if (c == 'y') mood = 3;
+            }       
+            
+        }
+        
+        int getmood() {
+            return mood;
+        }
+        String getname() {
+            return name;
+        }
+        
+        
     }
+   
+    
+    
+    public static void main(String[] args) {
+
+        System.out.println("Name");
+
+        Getgroove name1 = new Getgroove();
+        Getgroove name2 = new Getgroove("Suzie");
+        
+        
+        
+        System.out.println("Lets help " + name1.name + " get their groove back");
+        while (name1.getmood() != 3) {
+            name1.menu();
+        }
+        System.out.println("Good");
+        
+        System.out.println("");
+        
+        System.out.println("LETS MAKE " + name2.name + " HAPPY");
+        while (name2.getmood() != 3) {
+            name2.menu();
+        }
+        System.out.println("Good");
+       
+
+    }
+     
 }
